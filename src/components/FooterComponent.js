5199c61e05f60c78784437299344f9ad701d200e
loquence <ryandryv@gmail.com>
2019-01-03 22:40:40 -0500

    minor patch to fix linked in button

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -31,8 +31,7 @@ class Footer extends Component {
                             <br />
                             <h4 align="center" class="aboutme">Contacts: </h4>
                             <div className="text-center">
-                                <a className="btn btn-social-icon btn-linkedin m-1" 
-                                href="www.linkedin.com/in/ryan-kennedy-cs" id="linkedin"><i className="fa fa-linkedin"></i></a>
+                                <a className="btn btn-social-icon btn-linkedin m-1" href="http://www.linkedin.com/in/ryan-kennedy-cs/"id="linkedin"><i className="fa fa-linkedin"></i></a>
                                 <Tooltip placement="left" isOpen={this.state.tooltipOpen} target="linkedin" toggle={this.toggle}>Linked In</Tooltip>
                                 <a className="btn btn-social-igon btn-github m-1" id="github" href="https://github.com/loquence"><i className="fa fa-github"></i></a>
                                 <Tooltip placement="bottom" isOpen={this.state.tooltipOpen} target="github" toggle={this.toggle}>Github</Tooltip>
