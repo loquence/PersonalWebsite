9b6a0c9f37ae4a3780b0bb3a7b4e6167a6891b6e
loquence <ryandryv@gmail.com>
2019-01-05 13:59:55 -0500

    working on deployment

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -29,10 +29,10 @@ class Footer extends Component {
                     <div className="row justify-content-center">
                     <div className="col-12 col-sm-4 align-self-center">
                             <br />
-                            <h4 align="center" class="aboutme">Contacts: </h4>
+                            <h4 align="center" className="aboutme">Contacts: </h4>
                             <div className="text-center">
                                 <a className="btn btn-social-icon btn-linkedin m-1" href="http://www.linkedin.com/in/ryan-kennedy-cs/"id="linkedin"><i className="fa fa-linkedin"></i></a>
-                                <Tooltip placement="left" isOpen={this.state.tooltipOpen} target="linkedin" toggle={this.toggle}>Linked In</Tooltip>
+                                <Tooltip placement="left" isOpen={this.state.tooltipOpen}  target="linkedin" toggle={this.toggle}>Linked In</Tooltip>
                                 <a className="btn btn-social-igon btn-github m-1" id="github" href="https://github.com/loquence"><i className="fa fa-github"></i></a>
                                 <Tooltip placement="bottom" isOpen={this.state.tooltipOpen} target="github" toggle={this.toggle}>Github</Tooltip>
                                 <a className="btn btn-social-icon m-1" id="email" href="mailto:ryan.kennedy2@gmail.com"><i className="fa fa-envelope-o"></i></a>
