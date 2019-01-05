92ef88472a462fa7b54ff51afe335b4c46e7c2de
loquence <ryandryv@gmail.com>
2019-01-05 15:27:18 -0500

    please fucking work this time

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      
         <div className="App">
           <Main />
         </div>
-      </BrowserRouter>
+      
     );
   }
 }
